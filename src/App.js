2eb87becabb0b2bd40b8ba6858fb67c83e697bbf
Rub21 <rub2106@gmail.com>
2019-01-10 09:38:33 -0500

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import amber from '@material-ui/core/colors/amber';
 
