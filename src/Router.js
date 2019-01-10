9ce1b28b4d5dab4367c21f95b3ff7a2747a38f7f
Rub21 <rub2106@gmail.com>
2019-01-10 10:49:03 -0500

    Add about and 404 page

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { BrowserRouter as ReactRouter, Route } from 'react-router-dom';
+import { BrowserRouter as ReactRouter, Route, Switch } from 'react-router-dom';
 import { path } from './config';
 
 import App from './App';
 import Home from './views/Home';
+import Page404 from './views/Page404';
+import About from './views/About';
 
 export default class Router extends React.Component {
   render() {
     return (
       <ReactRouter basename={path}>
         <App>
-          <Route exact path="/" component={Home} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route component={Page404} />
+          </Switch>
         </App>
       </ReactRouter>
     );
