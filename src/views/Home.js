4782d9e7858dd1d67532a2abd19de205771e5ffc
Rub21 <rub2106@gmail.com>
2019-01-10 09:48:02 -0500

    fix the test

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>Data-seed</h1>
+      </div>
+    );
   }
 }
 
