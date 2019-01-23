a952c0804544e389ea87ffb3da1e6b4988b8abcc
Rub21 <rub2106@gmail.com>
2019-01-23 15:44:04 -0500

    Add sidebar-collased

--- app/assets/scripts/views/Home.js
+++ app/assets/scripts/views/Home.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 class Home extends Component {
   render () {
     return (
       <div className="container">
+        {/* sidebar-collased */}
         <Header/>
-
-        <nav>
-
-        </nav>
+        <Sidebar/>
 
         <main>
 
